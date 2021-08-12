f9ad8e16a3d6ccd5b4d64e76d55c37d054426115
Allyson Rampersad <allysonrampersad123@gmail.com>
2021-08-12 17:54:28 -0400

    Initial Commit

---
+++ p.py
@@ -0,0 +1,19 @@
+import os
+
+os.system('clear') 
+
+first = "Allyson Rampersad"
+age=21
+names=["Ally","Paul","Sally"]
+
+#dictionary key and value pair
+fav_pizza = {
+"John":"Pepperoni",
+"Bob":"Mushrooms",
+"Mary":"Cheese"
+}
+print (fav_pizza["John"])
+
+
+
+
